a879dab8eb0f16dcf22c65cdab39afb66da28f77
iciv <57459821+iciv@users.noreply.github.com>
2024-08-26 11:44:13 +0300

    Add missing oject deffinition

--- variables.tf
+++ variables.tf
@@ -6,7 +6,7 @@ variable "instance_type" {
 variable "ami_filter" {
   description = "Name filter and owner for AMI"
 
-  type = object({
+  type = object ({
     name  = string
     owner = string
   })
@@ -21,7 +21,7 @@ variable "ami_filter" {
 variable "environment" {
   description = "Development Environment"
 
-  type = ({
+  type = object ({
     name           = string
     network_prefix = string
   })

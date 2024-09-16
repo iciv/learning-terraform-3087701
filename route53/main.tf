6768c337849c45af9ab8a94c5252862efae5adcd
iciv <57459821+iciv@users.noreply.github.com>
2024-09-16 16:33:23 +0300

    Test for barzahlen.ch

--- route53/main.tf
+++ route53/main.tf
@@ -1,17 +1,17 @@
-# Create the Route 53 zone
-resource "aws_route53_zone" "barzahlen_at" {
-  name = "barzahlen.at"
+### Begin: Defaults
+# Specify a zone for a domain
+resource "aws_route53_zone" "barzahlen_ch" {
+  name = "barzahlen.ch"
 }
 
-# NS records are automatically created by Route 53, so we don't need to define them
+### End: Defaults
 
-# CAA Records
-resource "aws_route53_record" "barzahlen_at_caa" {
-  zone_id = aws_route53_zone.barzahlen_at.zone_id
+### Begin: Combined CAA configuration
+resource "aws_route53_record" "barzahlen_ch_caa_combined" {
+  zone_id = aws_route53_zone.barzahlen_ch.zone_id
   name    = ""
   type    = "CAA"
-  ttl     = 86400
-
+  ttl     = 3600
   records = [
     "0 issue \"digicert.com\"",
     "0 issue \"comodo.com\"",
@@ -20,70 +20,72 @@ resource "aws_route53_record" "barzahlen_at_caa" {
     "0 issue \"sectigo.com\""
   ]
 }
+### End: Combined CAA configuration
 
-# A record for controlcenter
-resource "aws_route53_record" "barzahlen_at_controlcenter_a" {
-  zone_id = aws_route53_zone.barzahlen_at.zone_id
+### Begin: Custom Entries
+# Handle A-record for controlcenter.<domain>.<tld>
+resource "aws_route53_record" "barzahlen_ch_controlcenter_a" {
+  zone_id = aws_route53_zone.barzahlen_ch.zone_id
   name    = "controlcenter"
   type    = "A"
   ttl     = 3600
   records = ["80.82.206.134"]
 }
 
-# CNAME record for autodiscover
-resource "aws_route53_record" "barzahlen_at_autodiscover_cname" {
-  zone_id = aws_route53_zone.barzahlen_at.zone_id
+# Handle CNAME-record for autodiscover.<domain>.<tld>
+resource "aws_route53_record" "barzahlen_ch_autodiscover_cname" {
+  zone_id = aws_route53_zone.barzahlen_ch.zone_id
   name    = "autodiscover"
   type    = "CNAME"
   ttl     = 3600
-  records = ["autodiscover.outlook.com"]
+  records = ["autodiscover.outlook.com."]
 }
 
-# CNAME record for selector1._domainkey
-resource "aws_route53_record" "barzahlen_at_selector1dkey_cname" {
-  zone_id = aws_route53_zone.barzahlen_at.zone_id
+# Handle CNAME-record for selector1._domainkey.<domain>.<tld>
+resource "aws_route53_record" "barzahlen_ch_selector1dkey_cname" {
+  zone_id = aws_route53_zone.barzahlen_ch.zone_id
   name    = "selector1._domainkey"
   type    = "CNAME"
   ttl     = 3600
-  records = ["selector1-barzahlen-at._domainkey.skrill.onmicrosoft.com"]
+  records = ["selector1-barzahlen-ch._domainkey.skrill.onmicrosoft.com."]
 }
 
-# CNAME record for selector2._domainkey
-resource "aws_route53_record" "barzahlen_at_selector2dkey_cname" {
-  zone_id = aws_route53_zone.barzahlen_at.zone_id
+# Handle CNAME-record for selector2._domainkey.<domain>.<tld>
+resource "aws_route53_record" "barzahlen_ch_selector2dkey_cname" {
+  zone_id = aws_route53_zone.barzahlen_ch.zone_id
   name    = "selector2._domainkey"
   type    = "CNAME"
   ttl     = 3600
-  records = ["selector2-barzahlen-at._domainkey.skrill.onmicrosoft.com"]
+  records = ["selector2-barzahlen-ch._domainkey.skrill.onmicrosoft.com."]
 }
 
-# MX record
-resource "aws_route53_record" "barzahlen_at_mx" {
-  zone_id = aws_route53_zone.barzahlen_at.zone_id
+# Handle MX-record for <domain>.<tld>
+resource "aws_route53_record" "barzahlen_ch_mx1" {
+  zone_id = aws_route53_zone.barzahlen_ch.zone_id
   name    = ""
   type    = "MX"
   ttl     = 3600
-  records = ["0 paysafe-com.mail.protection.outlook.com"]
+  records = ["0 paysafe-com.mail.protection.outlook.com."]
 }
 
-# Combined TXT records for the root domain
-resource "aws_route53_record" "barzahlen_at_txt" {
-  zone_id = aws_route53_zone.barzahlen_at.zone_id
+# Combined TXT record for SPF and Paysafe verification
+resource "aws_route53_record" "barzahlen_ch_txt_combined" {
+  zone_id = aws_route53_zone.barzahlen_ch.zone_id
   name    = ""
   type    = "TXT"
   ttl     = 3600
   records = [
-    "v=spf1 mx include:spf.protection.outlook.com ip4:5.45.109.84 ip4:37.200.98.157 ip4:46.231.176.212 ip4:46.231.176.222 ip6:2a03:4000:6:20f6::1 -all",
-    "qnyahgpqws3avvd5kcmwo6pbch9tv5yl",
-    "MS=ms63620310"
+    "v=spf1 mx include:spf.protection.outlook.com ip4:5.45.109.84 ip4:37.200.98.157 ip4:46.231.176.212 ip4:46.231.176.222 ip6:2a03:4000:6:20f6::1 -all",  # SPF record
+    "MS=ms40277327"  # Paysafe verification
   ]
 }
 
-# DMARC record (remains unchanged)
-resource "aws_route53_record" "barzahlen_at_dmarc" {
-  zone_id = aws_route53_zone.barzahlen_at.zone_id
+# DMARC TXT record (report-only)
+resource "aws_route53_record" "barzahlen_ch_dmarc" {
+  zone_id = aws_route53_zone.barzahlen_ch.zone_id
   name    = "_dmarc"
   type    = "TXT"
   ttl     = 3600
   records = ["v=DMARC1; p=reject; ruf=mailto:admin@viafintech.com; fo=1"]
 }
+### End: Custom Entries
---
+++ route53/outputs.tf
@@ -0,0 +1,3 @@
+output "barzahlen_at_nameservers" {
+  value = aws_route53_zone.barzahlen_at.name_servers
+}

3a978f27e71a164d92fb1752d66e9a754977cdbd
iciv <57459821+iciv@users.noreply.github.com>
2024-09-17 13:58:55 +0300

    cps_io

--- route53/main.tf
+++ route53/main.tf
@@ -1,15 +1,15 @@
 ### Begin: Defaults
 # Specify a zone for a domain
-resource "aws_route53_zone" "cashpaymentsolutions_pl" {
-  name = "cashpaymentsolutions.pl"
+resource "aws_route53_zone" "cps_io" {
+  name = "cps.io"
 }
 
 # NS records are automatically created by AWS Route 53, so no need to define them.
 ### End: Defaults
 
 ### Begin: Combined CAA configuration
-resource "aws_route53_record" "cashpaymentsolutions_pl_caa_combined" {
-  zone_id = aws_route53_zone.cashpaymentsolutions_pl.zone_id
+resource "aws_route53_record" "cps_io_caa_combined" {
+  zone_id = aws_route53_zone.cps_io.zone_id
   name    = ""
   type    = "CAA"
   ttl     = 3600
@@ -25,22 +25,91 @@ resource "aws_route53_record" "cashpaymentsolutions_pl_caa_combined" {
 
 ### Begin: Custom Entries
 
-# Handle Mail/SPF record
-# Disallows sending mails on behalf of cashpaymentsolutions.pl
-resource "aws_route53_record" "cashpaymentsolutions_pl_spf" {
-  zone_id = aws_route53_zone.cashpaymentsolutions_pl.zone_id
+# Handle A-records
+resource "aws_route53_record" "cps_io_a_records" {
+  zone_id = aws_route53_zone.cps_io.zone_id
+  for_each = {
+    "emailsignature"    = "94.130.121.74"
+    "fornax"            = "167.235.0.80"
+    "gems"              = "94.130.121.74"
+    "gw"                = "94.130.121.77"
+    "hermes"            = "94.130.121.74"
+    "hqgw"              = "217.111.25.34"
+    "incoming"          = "80.82.206.135"
+    "servicedesk"       = "94.130.121.78"
+    "spectral"          = "88.198.59.182"
+    "tas"               = "80.82.206.138"
+    "tass"              = "80.82.206.138"
+    "xbox"              = "88.99.148.153"
+    "xbox-services"     = "94.130.121.74"
+  }
+  name    = each.key
+  type    = "A"
+  ttl     = 3600
+  records = [each.value]
+}
+
+# Handle AAAA-records
+resource "aws_route53_record" "cps_io_aaaa_records" {
+  zone_id = aws_route53_zone.cps_io.zone_id
+  for_each = {
+    "emailsignature"    = "2a01:4f8:10a:2cd8::74"
+    "fornax"            = "2a01:4f8:262:1046::2"
+    "gems"              = "2a01:4f8:10a:2cd8::74"
+    "hermes"            = "2a01:4f8:10a:2cd8::74"
+    "servicedesk"       = "2a01:4f8:10a:2cd8::78"
+    "xbox"              = "2a01:4f8:10a:2cd8::2"
+    "xbox-services"     = "2a01:4f8:10a:2cd8::74"
+  }
+  name    = each.key
+  type    = "AAAA"
+  ttl     = 3600
+  records = [each.value]
+}
+
+# Handle CNAME records
+resource "aws_route53_record" "cps_io_cname_records" {
+  zone_id = aws_route53_zone.cps_io.zone_id
+  for_each = {
+    "docs.tech"         = "docs-tech-cps-io-paysafe-consumer-viafintech-doc-1e5b486dfa5301.pages.gitlab.paysafe.cloud."
+    "assets"            = "viafintech.snipe-it.io."
+    "autodiscover"      = "autodiscover.outlook.com."
+    "selector1._domainkey" = "selector1-cps-io._domainkey.skrill.onmicrosoft.com."
+    "selector2._domainkey" = "selector2-cps-io._domainkey.skrill.onmicrosoft.com."
+  }
+  name    = each.key
+  type    = "CNAME"
+  ttl     = 3600
+  records = [each.value]
+}
+
+# Handle MX-record
+resource "aws_route53_record" "cps_io_mx1" {
+  zone_id = aws_route53_zone.cps_io.zone_id
+  name    = ""
+  type    = "MX"
+  ttl     = 3600
+  records = ["0 paysafe-com.mail.protection.outlook.com."]
+}
+
+# Combined TXT records for SPF, DMARC, and DNSWL
+resource "aws_route53_record" "cps_io_txt_combined" {
+  zone_id = aws_route53_zone.cps_io.zone_id
   name    = ""
   type    = "TXT"
   ttl     = 3600
-  records = ["v=spf1 -all"]
+  records = [
+    "v=spf1 mx include:spf.protection.outlook.com ip4:5.45.109.84 ip4:37.200.98.157 ip4:46.231.176.212 ip4:46.231.176.222 ip4:94.130.121.72/29 ip6:2a01:4f8:10a:2cd8::/64 ip6:2a03:4000:6:20f6::1 ip6:2a00:1158:3::c7 -all",  # SPF
+    "v=DMARC1; p=reject; ruf=mailto:admin@viafintech.com; fo=1",  # DMARC
+    "cpy90m1nbqtjthf0f3ce3s40pyhbbitn"  # DNSWL
+  ]
 }
 
-# DMARC TXT record
-resource "aws_route53_record" "cashpaymentsolutions_pl_dmarc" {
-  zone_id = aws_route53_zone.cashpaymentsolutions_pl.zone_id
-  name    = "_dmarc"
+# DNS TXT Paysafe Verification
+resource "aws_route53_record" "cps_io_paysafe" {
+  zone_id = aws_route53_zone.cps_io.zone_id
+  name    = ""
   type    = "TXT"
   ttl     = 3600
-  records = ["v=DMARC1; p=reject; ruf=mailto:admin@viafintech.com; fo=1"]
+  records = ["MS=ms30858004"]
 }
-### End: Custom Entries

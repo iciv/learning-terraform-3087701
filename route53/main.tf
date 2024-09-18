30c261c6e7949c4932468668a1176d2cd11cc0b4
iciv <57459821+iciv@users.noreply.github.com>
2024-09-18 16:54:39 +0300

    viafintech_com

--- route53/main.tf
+++ route53/main.tf
@@ -4,15 +4,17 @@ resource "aws_route53_zone" "viafintech_com" {
   name = "viafintech.com"
 }
 
-# NS records are automatically created by AWS Route 53, so no need to define them.
+# Note: AWS Route 53 automatically creates NS records for the zone,
+# so we don't need to specify them explicitly.
+
 ### End: Defaults
 
-### Begin: Combined CAA configuration
-resource "aws_route53_record" "viafintech_com_caa_combined" {
+### Begin: CAA configuration
+resource "aws_route53_record" "viafintech_com_caa" {
   zone_id = aws_route53_zone.viafintech_com.zone_id
   name    = ""
   type    = "CAA"
-  ttl     = 3600
+  ttl     = 86400
   records = [
     "0 issue \"digicert.com\"",
     "0 issue \"comodo.com\"",
@@ -22,91 +24,188 @@ resource "aws_route53_record" "viafintech_com_caa_combined" {
     "0 issue \"amazon.com\""
   ]
 }
-### End: Combined CAA configuration
+### End: CAA configuration
+
+### Custom Entries
 
 ### Begin: Custom Entries
-
-# Handle A-record for <domain>.<tld>
-resource "aws_route53_record" "viafintech_com_domainwide_a" {
+# Handle A-records for various subdomains
+resource "aws_route53_record" "viafintech_com_a_records" {
   zone_id = aws_route53_zone.viafintech_com.zone_id
-  name    = ""
+  for_each = {
+    ""                    = "94.130.121.74"
+    "api"                 = "46.231.176.214"
+    "api-sandbox"         = "46.231.176.215"
+    "api-client"          = "80.82.206.133"
+    "api-passkit"         = "80.82.206.136"
+    "api-passkit-sandbox" = "80.82.206.136"
+    "api-retail"          = "46.231.176.217"
+    "api-retail-sandbox"  = "46.231.176.216"
+    "api-utility"       = "46.231.176.218"
+    "api-utility-newcert" = "46.231.176.219"
+    "app" = "80.82.206.137"
+    "asgard" = "88.198.59.172"
+    "cavm" = "49.12.187.118"
+    "docs" = "159.69.116.223"
+    "stores" = "80.82.206.131"
+    "thor" = "88.198.59.172"
+    "transfer" = "46.231.176.221"
+  }
+  name    = each.key
   type    = "A"
   ttl     = 3600
-  records = ["94.130.121.74"]
+  records = [each.value]
 }
 
-# Handle A-record for api.<domain>.<tld>
-resource "aws_route53_record" "viafintech_com_api_a" {
+# Define a map for the AAAA records
+resource "aws_route53_record" "viafintech_com_aaaa_records" {
   zone_id = aws_route53_zone.viafintech_com.zone_id
-  name    = "api"
-  type    = "A"
-  ttl     = 3600
-  records = ["46.231.176.214"]
-}
 
-# Handle A-record for api-sandbox.<domain>.<tld>
-resource "aws_route53_record" "viafintech_com_apisandbox_a" {
-  zone_id = aws_route53_zone.viafintech_com.zone_id
-  name    = "api-sandbox"
-  type    = "A"
-  ttl     = 3600
-  records = ["46.231.176.215"]
-}
+  for_each = {
+    ""      = "2a01:4f8:10a:2cd8::74"
+    "asgard" = "2a01:4f8:222:784:6969::1"
+    "cavm"  = "2a01:4f8:c012:f7f1::1"
+    "docs"  = "2a01:4f8:c012:aec3::1"
+    "thor"  = "2a01:4f8:222:784::7468:254"
+  }
 
-# Handle AAAA-record for <domain>.<tld>
-resource "aws_route53_record" "viafintech_com_domainwide_aaaa" {
-  zone_id = aws_route53_zone.viafintech_com.zone_id
-  name    = ""
+  name    = each.key
   type    = "AAAA"
   ttl     = 3600
-  records = ["2a01:4f8:10a:2cd8::74"]
+  records = [each.value]
 }
 
 # Handle CNAME-record for autodiscover.<domain>.<tld>
-resource "aws_route53_record" "viafintech_com_autodiscover_cname" {
+resource "aws_route53_record" "viafintech_com_cname_records" {
   zone_id = aws_route53_zone.viafintech_com.zone_id
-  name    = "autodiscover"
+
+  for_each = {
+    "autodiscover"                        = "autodiscover.outlook.com."
+    "selector1._domainkey"                = "selector1-viafintech-com._domainkey.skrill.onmicrosoft.com."
+    "selector2._domainkey"                = "selector2-viafintech-com._domainkey.skrill.onmicrosoft.com."
+    "cdn"                                 = "viafintech-4bb3.kxcdn.com."
+    "helpdesk"                            = "barzahlen.zammad.com."
+    "ldap"                                = "thor"
+    "wsdl"                                = "vfsoap-4bb3.kxcdn.com."
+    "www"                                 = "dkinrlus7agur.cloudfront.net."
+    "www.app"                             = "app"
+    "mailtracking"                        = "mandrillapp.com."
+    "bnc3"                                = "bnc3.mailjet.com."
+    "k2._domainkey"                       = "dkim2.mcsv.net."
+    "k3._domainkey"                       = "dkim3.mcsv.net."
+    "mte1._domainkey"                     = "dkim1.mandrillapp.com."
+    "mte2._domainkey"                     = "dkim2.mandrillapp.com."
+    "_e34b416eb07936323962d436b3cc1ee9"   = "_e3698f5b18c45fca990e3e47c3207dc7.mhbtsbpdnt.acm-validations.aws."
+    "_92d8b9c6e3b0f4dd3e00226a64cf397b.www" = "_45484436cf957a4918dfd374b023fc90.mhbtsbpdnt.acm-validations.aws."
+  }
+
+  name    = each.key
   type    = "CNAME"
   ttl     = 3600
-  records = ["autodiscover.outlook.com."]
+  records = [each.value]
 }
 
-# Handle MX-record for <domain>.<tld>
-resource "aws_route53_record" "viafintech_com_mx1" {
+
+
+
+
+resource "aws_route53_record" "viafintech_com_mx_txt_dkim_records" {
   zone_id = aws_route53_zone.viafintech_com.zone_id
-  name    = ""
-  type    = "MX"
-  ttl     = 3600
-  records = ["0 viafintech-com.mail.protection.outlook.com."]
-}
 
-# Handle Mail/SPF record
-resource "aws_route53_record" "viafintech_com_spf" {
-  zone_id = aws_route53_zone.viafintech_com.zone_id
-  name    = ""
-  type    = "TXT"
-  ttl     = 3600
-  records = [
-    "v=spf1 a mx include:spf.protection.outlook.com include:_spf.viafintech.com include:spf.mandrillapp.com include:spf.mailjet.com include:_spf.salesforce.com -all"
-  ]
-}
+  for_each = {
+    # MX Record
+    "MX" = {
+      name    = ""
+      type    = "MX"
+      ttl     = 3600
+      records = ["0 viafintech-com.mail.protection.outlook.com."]
+    }
 
-# DMARC TXT record
-resource "aws_route53_record" "viafintech_com_dmarc" {
-  zone_id = aws_route53_zone.viafintech_com.zone_id
-  name    = "_dmarc"
-  type    = "TXT"
-  ttl     = 3600
-  records = ["v=DMARC1; p=reject; ruf=mailto:admin@viafintech.com; fo=1"]
-}
+    # SPF Records
+    "SPF1" = {
+      name    = ""
+      type    = "TXT"
+      ttl     = 3600
+      records = [jsonencode("v=spf1 a mx include:spf.protection.outlook.com include:_spf.viafintech.com include:spf.mandrillapp.com include:spf.mailjet.com include:_spf.salesforce.com -all")]
+    }
+    "SPF2" = {
+      name    = "_spf"
+      type    = "TXT"
+      ttl     = 3600
+      records = [jsonencode("v=spf1 include:_netblocks.viafintech.com include:_netblocks2.viafintech.com -all")]
+    }
+    "SPF3" = {
+      name    = "_netblocks"
+      type    = "TXT"
+      ttl     = 3600
+      records = [jsonencode("v=spf1 ip4:46.231.176.208/28 ip4:80.82.206.128/26 ip4:88.99.148.153 ip4:94.130.121.72/29 ip4:217.111.25.32/29 -all")]
+    }
+    "SPF4" = {
+      name    = "_netblocks2"
+      type    = "TXT"
+      ttl     = 3600
+      records = [jsonencode("v=spf1 ip6:2a00:cd0:1025::/48 ip6:2a01:4f8:10a:2cd8::/64 ip6:2001:920:1a21:2900::/56 -all")]
+    }
 
-# Google Site Verification
-resource "aws_route53_record" "viafintech_com_google_siteverification" {
-  zone_id = aws_route53_zone.viafintech_com.zone_id
-  name    = ""
-  type    = "TXT"
-  ttl     = 3600
-  records = ["google-site-verification=fVi_Aw06nFcMZGt4-cDzZa0Bc6yXlrEP41j5n6Eza-I"]
-}
+    # DMARC Records
+    "DMARC" = {
+      name    = "_dmarc"
+      type    = "TXT"
+      ttl     = 3600
+      records = [jsonencode("v=DMARC1; p=reject; ruf=mailto:admin@viafintech.com; fo=1")]
+    }
+    "DMARC_OTHERS" = {
+      name    = "*._report._dmarc"
+      type    = "TXT"
+      ttl     = 3600
+      records = [jsonencode("v=DMARC1")]
+    }
 
-### End: Custom Entries
+    # Verification Records
+    "GOOGLE_SITE_VERIFICATION" = {
+      name    = ""
+      type    = "TXT"
+      ttl     = 3600
+      records = ["google-site-verification=fVi_Aw06nFcMZGt4-cDzZa0Bc6yXlrEP41j5n6Eza-I"]
+    }
+    "MAILJET_VERIFICATION" = {
+      name    = "mailjet._6323429f"
+      type    = "TXT"
+      ttl     = 3600
+      records = [jsonencode("6323429f5b5f674fb4391252496cd2af")]
+    }
+
+    # DKIM Records
+    "MAILJET_DKIM" = {
+      name    = "mailjet._domainkey"
+      type    = "TXT"
+      ttl     = 3600
+      records = [jsonencode("v=DKIM1; k=rsa; p=MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCdPO28UO2uUVUXvGEI/XfmS92UqmbiXTcUX+ZzT0cpeKNi6kAiAfgIi+Y3faPlcJCzii83pQJWVyvCihlxqo9YVyyWBVsP5ycHC3CNmt/eLuBv/AAnONwUrEiVwbdSO1Ty0DQJXlc85TpiaknXr41MzkNDAlabX97ZHMWJzFf2rwIDAQAB")]
+    }
+    "MANDRILL_DKIM" = {
+      name    = "mandrill._domainkey"
+      type    = "TXT"
+      ttl     = 3600
+      records = [jsonencode("v=DKIM1; k=rsa; p=MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCrLHiExVd55zd/IQ/J/mRwSRMAocV/hMB3jXwaHH36d9NaVynQFYV8NaWi69c1veUtRzGt7yAioXqLj7Z4TeEUoOLgrKsn8YnckGs9i3B3tVFB+Ch/4mPhXWiNfNdynHWBcPcbJ8kjEQ2U8y78dHZj1YeRXXVvWob2OaKynO8/lQIDAQAB;")]
+    }
+    "HETZNER_DKIM" = {
+      name    = "default2205._domainkey"
+      type    = "TXT"
+      ttl     = 3600
+      records = [jsonencode("v=DKIM1;p=MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAzluwhgUUAN+pWCtRDry5u5u02lULd0UX/M0tli3LG4+0BN7TTaZU5VJH58HIofGYM0bqKSjP5sUiRI+5g59774maQg7hSinhp9kCVRgCGU0nOLaK1uLrRjCnavTOcyU6369AfPHj/Ip9AwuP3WiUSzeHUHr3ZRUSsvkX710y3352AhBnV+JxnH4BA1z+YkNF9nfBrTMTon1OnPRDo8a0OknMuVem15Kg/s4Rm4bMBfio+16Al1qplM+mXeJW36ExZgr+C2nrERzmKEMPWhD5WBqq8ipXm9L830w6AWkExW/2/G0bIdieZ0FUU1OEafLZz5y3+UTED+cTtcwdsIIJ6wIDAQAB")]
+    }
+
+    # Paysafe Verification
+    "PAYSAFE" = {
+      name    = ""
+      type    = "TXT"
+      ttl     = 3600
+      records = [jsonencode("MS=ms20420367")]
+    }
+  }
+
+  name    = each.value.name
+  type    = each.value.type
+  ttl     = each.value.ttl
+  records = each.value.records
+}
